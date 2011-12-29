1c00836d8ea4e38c853ee3ac8d502aa615cfaee4
johnjbarton <johnjbarton@johnjbarton.com>
2011-12-29 11:30:29 -0800

    report events up to test window

--- test/DemoDebugger.js
+++ test/DemoDebugger.js
@@ -1,9 +1,13 @@
 // See Purple/license.txt for Google BSD license
 // Copyright 2011 Google, Inc. johnjbarton@johnjbarton.com
 
-/*global define console */
+/*global define console window */
 define(  ['ScriptDebuggerProxy'], 
   function(ScriptDebuggerProxy) {
+  
+  function output() {
+    window.parent.postMessage(arguments,"*");
+  }
   
   var DemoDebugger = ScriptDebuggerProxy.extend({
   
@@ -11,30 +15,30 @@ define(  ['ScriptDebuggerProxy'],
     eventHandlers: {
       Debugger: {
         breakpointResolved: function(breakpointId, location) {
-          console.log("DemoDebugger", arguments);
+          output("DemoDebugger", arguments);
         },
         paused: function(details) {
-          console.log("DemoDebugger paused", arguments);
+          output("DemoDebugger paused", arguments);
         },
         resumed: function() {
-          console.log("DemoDebugger", arguments);
+          output("DemoDebugger", arguments);
         },
         scriptFailedToParse: function(data, errorLine, errorMessage, firstLine, url) {
-          console.log("DemoDebugger", arguments);
+          output("DemoDebugger", arguments);
         },
         scriptParsed: function(endColumn, endLine, isContentScript, scriptId, startColumn, startLine, url, p_id) {
-          console.log('scriptParsed '+url);
+          output('scriptParsed '+url);
         }
       },
       Timeline: {
         eventRecorded: function(record) {
-          console.log("DemoDebugger", arguments);
+          output("DemoDebugger", arguments);
         },
         started: function() {
-          console.log("DemoDebugger", arguments);
+          output("DemoDebugger", arguments);
         },
         stopped: function() {
-          console.log("DemoDebugger", arguments);
+          output("DemoDebugger", arguments);
         }
       }
     },
